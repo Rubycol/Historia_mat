8356728860bdc0e5c7748042b1cad6ae15c64224
Rubby Colonia <rubycolonia@gmail.com>
2021-07-17 00:43:50 -0500

    comité 7

---
+++ Historia_mat/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Historia_mat
+//
+//  Created by RUBBY on 16/07/21.
+//  Copyright © 2021 RUBBY. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Historia_mat/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Historia_mat
+//
+//  Created by RUBBY on 16/07/21.
+//  Copyright © 2021 RUBBY. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
